32979a4943afa5e49dd53b864d8e219329700b1f
2417 <w2014@e-mirim.hs.kr>
2022-03-21 14:50:38 +0900

    views: {'name': '이름'} -> templates: {{ name }}

--- playground/views.py
+++ playground/views.py
@@ -6,7 +6,7 @@ def say_hello(request):
     return HttpResponse("Hello, World!")
 
 def say_hello_html(request):
-    return render(request, 'playground/hello.html')
+    return render(request, 'playground/hello.html', {'name': '해인홍'})
 
 def say_bye(request):
     return render(request, 'playground/bye.html')
